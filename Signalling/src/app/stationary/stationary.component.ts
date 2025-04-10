10fbe00f287b36cc47e2e429f906c1d981e0927b
Rituparna Gotwig <98028967+RituGotwig@users.noreply.github.com>
2025-04-10 08:43:58 +0200

    first layout

---
+++ Signalling/src/app/stationary/stationary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stationary',
+  standalone: true,
+  imports: [],
+  templateUrl: './stationary.component.html',
+  styleUrl: './stationary.component.css'
+})
+export class StationaryComponent {
+
+}
